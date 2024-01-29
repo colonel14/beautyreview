67459b58f07d7348456dd60770adcedf5c153376
Abdallah <abdallah.mohamed.conan@gmail.com>
2024-01-30 00:41:49 +0200

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/profile", "/dashboard", "/product/:path*", "/new-password"],
+  matcher: ["/profile", "/dashboard", "/product/:path*"],
 };
